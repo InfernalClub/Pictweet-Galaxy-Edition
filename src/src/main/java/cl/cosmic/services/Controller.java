2462bb117f79603c7aaf01154efeafb0db93e8b0
InfernalClub <kamitsuki_lord_of_blood@hotmail.com>
2024-12-03 16:37:36 -0300

    fixed and added modules

--- src/src/main/java/cl/cosmic/services/Controller.java
+++ src/src/main/java/cl/cosmic/services/Controller.java
@@ -1,11 +1,17 @@
 package cl.cosmic.services;
 
 import cl.cosmic.model.Persona;
-import cl.cosmic.model.Pic;  // Corrected to use cl.cosmic.model
-import cl.cosmic.model.PicTwin;  // Corrected to use cl.cosmic.model
+import cl.cosmic.model.Pic;
+import cl.cosmic.model.PicTwin;
+import cl.cosmic.model.query.QPersona;
+import cl.cosmic.model.query.QPicTwin;
+import cl.cosmic.utils.FileUtils;  // Assuming you have a utility class for file operations
 import com.password4j.Password;
 import io.ebean.Database;
 import io.ebean.annotation.Transactional;
+import java.io.File;
+import java.time.Instant;
+import java.util.List;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,6 +24,24 @@ public class Controller {
     /** The Constructor. */
     public Controller(@NonNull final Database database) {
         this.database = database;
+    }
+
+    /** The seed of the database. */
+    public Boolean seed() {
+        // Find the Persona count
+        int personaCount = new QPersona().findCount();
+        log.debug("Personas in database: {}", personaCount);
+
+        // If Persona exists, don't seed
+        if (personaCount > 0) {
+            return Boolean.FALSE;
+        }
+
+        // Seed the database
+        Persona persona = this.register("durrutia@ucn.cl", "durrutia123");
+        log.debug("Persona registered: {}", persona);
+
+        return Boolean.TRUE;
     }
 
     /** Register a new user. */
@@ -41,4 +65,64 @@ public class Controller {
 
         return persona;
     }
+
+    /** Login a user. */
+    public Persona login(@NonNull final String email, @NonNull final String password) {
+        // Find Persona by email
+        Persona persona = new QPersona().email.equalTo(email).findOne();
+        if (persona == null) {
+            throw new RuntimeException("User not found");
+        }
+
+        // Check if the password is correct
+        if (!Password.check(password, persona.getPassword()).withBcrypt()) {
+            throw new RuntimeException("Wrong password");
+        }
+
+        return persona;
+    }
+
+    /** Add a new Pic. */
+    @Transactional
+    public PicTwin addPic(@NonNull String ulidPersona, @NonNull Double latitude, @NonNull Double longitude, @NonNull File picture) {
+        // Read the file
+        byte[] data = FileUtils.readAllBytes(picture);
+
+        // Find the Persona
+        Persona persona = new QPersona().ulid.equalTo(ulidPersona).findOne();
+        log.debug("Persona found: {}", persona);
+
+        // Save the Pic
+        Pic pic = Pic.builder()
+                .latitude(latitude)
+                .longitude(longitude)
+                .reports(0)
+                .date(Instant.now())
+                .photo(data)
+                .bloqued(false)
+                .views(0)
+                .persona(persona)
+                .build();
+        this.database.save(pic);
+        log.debug("Pic saved: {}", pic);
+
+        // Save the PicTwin
+        PicTwin picTwin = PicTwin.builder()
+                .expiration(Instant.now().plusSeconds(7 * 24 * 60 * 60)) // 1 week expiration
+                .expired(false)
+                .reported(false)
+                .persona(persona)
+                .pic(pic)
+                .twin(pic) // FIXME: retrieve a new pic from the database
+                .build();
+        this.database.save(picTwin);
+
+        return picTwin;
+    }
+
+    /** Get the PicTwins. */
+    public List<PicTwin> getPicTwins(@NonNull String ulidPersona) {
+        return new QPicTwin().persona.ulid.equalTo(ulidPersona).findList();
+    }
 }
+
